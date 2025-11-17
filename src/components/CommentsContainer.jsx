a0cb91130bd5b2b3e49fd3e84edc4c2d71db82a3
syedasadhashmi <asadhashmi22@gmail.com>
2025-11-18 01:36:31 +0500

    nested comments

---
+++ src/components/CommentsContainer.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+
+const commentsData = [
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "Akshay Saini",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [
+              {
+                name: "Akshay Saini",
+                text: "Lorem ipsum dolor sit amet, consectetur adip",
+                replies: [
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [
+                      {
+                        name: "Akshay Saini",
+                        text: "Lorem ipsum dolor sit amet, consectetur adip",
+                        replies: [],
+                      },
+                    ],
+                  },
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, text } = data;
+
+  return (
+    <div className="flex bg-gray-100 p-2 rounde-lg shadow-sm m-2">
+      <FaUserCircle size={42} />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentList = ({ comments }) => {
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
+const CommentsContainer = () => {
+  return (
+    <div className="m-5 p-2">
+      <h1 className="text-2xl font-bold">Comments</h1>
+      <CommentList comments={commentsData} />
+    </div>
+  );
+};
+
+export default CommentsContainer;
--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router";
+import CommentsContainer from "./CommentsContainer";
 
 const Watch = () => {
   const [searchParams] = useSearchParams();
@@ -13,17 +14,20 @@ const Watch = () => {
   }, []);
 
   return (
-    <div className="p-5">
-      <iframe
-        width="900"
-        height="450"
-        src={`https://www.youtube.com/embed/${videoId}`}
-        title="YouTube video player"
-        // frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        // referrerpolicy="strict-origin-when-cross-origin"
-        // allowFullscreen
-      ></iframe>
+    <div>
+      <div className="p-5">
+        <iframe
+          width="900"
+          height="450"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          // frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          // referrerpolicy="strict-origin-when-cross-origin"
+          // allowFullscreen
+        ></iframe>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
